feb0385f7e546048ccca63b8588f9405f6cb596e
Sara Coren <saracoren@Saras-MacBook-Air.local>
2019-05-29 12:26:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			quotes: []
+    }
+	}
+	componentDidMount() {
+		this.getQuotes()
+	}
+	getQuotes () {
+		fetch('http://localhost:3000/quotes')
+			.then(response => response.json())
+			.then(json => this.setState({quotes: json}))
+      .catch(error => console.error(error))
+	}
+  render () {
+    return (
+      <div className = "container">
+        <h1> Notable Quotables</h1>
+    {this.state.quotes.map ( quote => {
+      return (
+        <div className='quotes' key={quote.id}>
+          <h3>Quote: {quote.phrase}</h3>
+          <h3>Author: {quote.author}</h3>
+        </div>
+      )
+    })}
+      </div>
+    )
+  }
 }
 
 export default App;
